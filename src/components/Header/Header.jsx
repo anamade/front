bd84d9621164c0298a907b58faf974bb998c46c7
anamade <50694980+anamade@users.noreply.github.com>
2025-01-25 19:11:44 +0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis, neque ipsum iste soluta eum eaque et optio reprehenderit sapiente quaerat aperiam! Ratione illum reprehenderit eligendi quaerat exercitationem officiis eos.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
